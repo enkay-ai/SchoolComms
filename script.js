c997d6f3c645f2b6e1f3427b67db186ec6f75d07
enkay-ai <147736745+enkay-ai@users.noreply.github.com>
2026-06-07 00:43:30 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_URL = "PASTE_YOUR_APPS_SCRIPT_WEB_APP_URL_HERE";
+const API_URL = "https://script.google.com/macros/s/AKfycbxCwZP42LymO57XP6-Z33Ckkc7EgpoaHk1lmobcJhF7PBpd9aWratzw3ENpnEsxU981/exec";
 
 let currentTab = "class";
 let currentUser = null;
